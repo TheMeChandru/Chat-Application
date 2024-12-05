631a2102a108e70b6324f64340edfdca0131bf5b
ChandraShekar <chandrashekar93802003@gmail.com>
2024-12-05 14:14:53 +0530

    Initial Push

---
+++ public/src/components/ChatContainer.jsx
@@ -0,0 +1,207 @@
+import { useEffect, useState,useRef } from 'react';
+import styled from 'styled-components'
+import Logout from './Logout'
+import ChatInput from './ChatInput'
+import Messages from './Messages'
+import axios from 'axios'
+import { v4 as uuidv4 } from "uuid";
+import { sendMessageRoute, recieveMessageRoute } from "../utils/APIRoutes";
+
+
+export default function ChatContainer({ currentChat,socket }) {
+  const [currentUser, setCurrentUser] = useState(null);
+  const [messages, setMessages] = useState([]);
+  const [arrivalMessage, setArrivalMessage] = useState(null);
+  const scrollRef = useRef();
+
+  useEffect(() => {
+    
+    const fetchMessages = async () => {
+      try {
+       
+        const data = JSON.parse(localStorage.getItem("chat-app-user"));
+
+        
+        if (!data || !data._id) {
+          console.error("User data is missing or invalid in localStorage");
+          return;
+        }
+
+        if(currentChat){
+
+          const response = await axios.post(recieveMessageRoute, {
+            from: data._id,
+            to: currentChat._id,
+          });
+          setMessages(response.data);
+        }
+        } catch (error) {
+          console.error("Error fetching messages:", error);
+        }
+    };
+
+   
+    fetchMessages();
+
+  }, [currentChat]);
+
+
+
+  useEffect(() => {
+    const userData = JSON.parse(localStorage.getItem("chat-app-user"));
+    // console.log('User data from localStorage:', userData);
+
+    if (userData && userData._id) {
+      setCurrentUser(userData);
+    } else {
+      console.error("No user data found in localStorage or user is missing an _id");
+    }
+  }, []);
+
+  const handleSendMsg = async (msg) => {
+    const data = await JSON.parse(
+      localStorage.getItem("chat-app-user")
+    );
+    socket.current.emit("send-msg", {
+      to: currentChat._id,
+      from: data._id,
+      msg,
+    });
+    await axios.post(sendMessageRoute, {
+      from: data._id,
+      to: currentChat._id,
+      message: msg,
+    });
+
+    const msgs = [...messages];
+    msgs.push({ fromSelf: true, message: msg });
+    setMessages(msgs);
+  };
+
+  useEffect(() => {
+    if (socket.current) {
+      socket.current.on("msg-recieve", (msg) => {
+        setArrivalMessage({ fromSelf: false, message: msg });
+      });
+    }
+  }, []);
+
+  useEffect(() => {
+    arrivalMessage && setMessages((prev) => [...prev, arrivalMessage]);
+  }, [arrivalMessage]);
+
+  useEffect(() => {
+    scrollRef.current?.scrollIntoView({ behavior: "smooth" });
+  }, [messages]);
+
+
+
+  return (
+    <>
+      {currentChat && (
+        <Container>
+          <div className='chat-header'>
+            <div className='user-details'>
+              <div className='avatar'>
+                <img src={currentChat.avatarImage} alt='User Avatar' />
+              </div>
+              <div className='username'>
+                <h3>{currentChat.username}</h3>
+              </div>
+            </div>
+            <Logout />
+          </div>
+          <div className="chat-messages">
+            {messages.map((message) => {
+              return (
+                <div ref={scrollRef} key={uuidv4()}>
+                <div key={message._id || message.timestamp}> 
+                  <div className={`message ${message.fromSelf ? "sended" : "recieved"}`}>
+                    <div className="content">
+                      <p>{message.message}</p>
+                    </div>
+                  </div>
+                </div>
+                </div>
+              );
+            })}
+          </div>
+          <ChatInput handleSendMsg={handleSendMsg} />
+        </Container>
+      )}
+    </>
+  )
+}
+
+const Container = styled.div`
+  display: grid;
+  grid-template-rows: 10% 80% 10%;
+  gap: 0.1rem;
+  overflow: hidden;
+  @media screen and (min-width: 720px) and (max-width: 1080px) {
+    grid-template-rows: 15% 70% 15%;
+  }
+  .chat-header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 0 2rem;
+    .user-details {
+      display: flex;
+      align-items: center;
+      gap: 1rem;
+      .avatar {
+        img {
+          height: 3rem;
+        }
+      }
+      .username {
+        h3 {
+          color: white;
+        }
+      }
+    }
+  }
+  .chat-messages {
+    padding: 1rem 2rem;
+    display: flex;
+    flex-direction: column;
+    gap: 1rem;
+    overflow: auto;
+    &::-webkit-scrollbar {
+      width: 0.2rem;
+      &-thumb {
+        background-color: #ffffff39;
+        width: 0.1rem;
+        border-radius: 1rem;
+      }
+    }
+    .message {
+      display: flex;
+      align-items: center;
+      .content {
+        max-width: 40%;
+        overflow-wrap: break-word;
+        padding: 1rem;
+        font-size: 1.1rem;
+        border-radius: 1rem;
+        color: #d1d1d1;
+        @media screen and (min-width: 720px) and (max-width: 1080px) {
+          max-width: 70%;
+        }
+      }
+    }
+    .sended {
+      justify-content: flex-end;
+      .content {
+        background-color: #4f04ff21;
+      }
+    }
+    .recieved {
+      justify-content: flex-start;
+      .content {
+        background-color: #9900ff20;
+      }
+    }
+  }
+`
---
+++ server/controllers/messagesController.js
@@ -0,0 +1,44 @@
+const mongoose = require("mongoose");
+const Messages = require("../models/messageModel");
+
+module.exports.getMessages = async (req, res, next) => {
+  try {
+    const { from, to } = req.body;
+
+    const messages = await Messages.find({
+      users: {
+        $all: [from, to],
+      },
+    }).sort({ updatedAt: 1 });
+
+    const projectedMessages = messages.map((msg) => {
+      return {
+        fromSelf: msg.sender.toString() === from,
+        message: msg.message.text,
+      };
+    });
+    res.json(projectedMessages);
+  } catch (ex) {
+    next(ex);
+  }
+};
+
+module.exports.addMessage = async (req, res, next) => {
+    try {
+      const { from, to, message } = req.body;
+  
+      
+      const data = await Messages.create({
+        message: { text: message },
+        users: [new mongoose.Types.ObjectId(from), new mongoose.Types.ObjectId(to)], 
+        sender: new mongoose.Types.ObjectId(from),
+      });
+  
+      if (data) return res.json({ msg: "Message added successfully." });
+      else return res.json({ msg: "Failed to add message to the database" });
+    } catch (ex) {
+      console.error(ex);
+      next(ex);
+    }
+  };
+  
